7fc6d3beb82e9acc345e5b5f9f0e38113464c9b8
Malte <wombatconrad@gmail.com>
2023-04-15 19:47:29 +0200

    css stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,27 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: 'Malte Nilsson || .NET Developer',
+    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `icons`,
+        path: `${__dirname}/src/images/icons`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `education`,
+        path: `${__dirname}/src/components/education`
+      }
+    }
+  ]
 }
