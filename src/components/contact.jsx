c2bfb6d9115779dd9a3d8e84c52827dbd6cba0d1
TheWombatKonrad <wombatconrad@gmail.com>
2023-05-19 11:51:41 +0200

    made linkedin link a photo

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,12 +1,24 @@
 import * as React from 'react'
 import { infoBox } from './layout.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Contact = () => {
   return (
     <div className={infoBox}>
       <h3>Contact Me</h3>
       <p>Email: malte.nilsson95@gmail.com</p>
-      <p>LinkedIn:<a href='https://www.linkedin.com/in/malte-nilsson/' target="_blank" rel="noreferrer">Malte Nilsson</a></p>
+      <ul style={{display: 'flex', listStyleType: 'none', marginLeft: '-40px' }}>
+        <li>
+          <a href='https://www.linkedin.com/in/malte-nilsson/' target="_blank" rel="noreferrer"> 
+            <StaticImage 
+            alt='LinkedIn'
+            src='../images/linkedin-icon-original.png'
+            aspectRatio={1 / 1}
+            loading='blurred'
+            style={{width: '20px'}}/>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -7,7 +7,13 @@ const Skills = ({ data }) => {
   return (
     <div className={infoBox}>
       <h3>Skills</h3>
-      <ul style={{ listStyleType: 'none', display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap', marginLeft: '-30px' }}>
+      <ul style={{ 
+        listStyleType: 'none', 
+        display: 'flex', 
+        justifyContent: 'space-evenly', 
+        flexWrap: 'wrap', 
+        marginLeft: '-40px' 
+        }}>
         {data.map((node) => (node.childImageSharp != null && (
           <li key={node.name} style={{margin: '2px'}}>
             <GatsbyImage
