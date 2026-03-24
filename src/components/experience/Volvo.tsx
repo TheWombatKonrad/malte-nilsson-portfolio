1afd4ac1b59cb18c417d90abac3f8fcd635e3350
TheWombatConrad <wombatconrad@gmail.com>
2026-03-24 21:28:39 +0100

    updated volvo internship

--- src/components/experience/Volvo.tsx
+++ src/components/experience/Volvo.tsx
@@ -1,16 +1,18 @@
-import ExpandableDiv from "../ExpandableDiv";
+import ExpandableDiv from '../ExpandableDiv';
 
 const BiologyMaster = () => {
   return (
     <ExpandableDiv
-      title={"Intership at Volvo"}
-      date={"February 2023 - May 2023"}
-      id="volvo"
+      title={'Intership at Volvo'}
+      date={'February 2023 - May 2023'}
+      id='volvo'
     >
       <p>
-        I did an intership at Volvo where I migrated a part of an existing
+        I did an intership at Volvo where I migrated part of an existing
         application from AngularJS to React, integrating the new React component
-        in the old code.
+        in the old code. We worked with Scrum, and the project involved working
+        with React on the frontend and C# on the backend. The project had mock
+        servers and additional services set up with Kubernetes and Docker.
       </p>
     </ExpandableDiv>
   );
