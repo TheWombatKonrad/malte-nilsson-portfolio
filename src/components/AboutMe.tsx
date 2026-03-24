511451aee48f04660d9f87347e4ef65f62d9fa51
TheWombatConrad <wombatconrad@gmail.com>
2026-03-24 20:53:24 +0100

    updated the about me info

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,11 +1,14 @@
 const AboutMe = () => {
   return (
-    <div className="info-box">
+    <div className='info-box'>
       <h3>About Me</h3>
       <p>
-        I recently graduated from Teknikhögskolan where I studied to become a
-        .NET developer. I'm inquisitive and am looking to develop my skills as a
-        programmer. Currently, I am working for VTD.
+        I am currently working for PostNord as a mailmain. Before that, I've
+        studied biology and programming. I'm inquisitive and am always learning
+        new things. My greatest strenght is that I'm very organised, my greatest
+        weakness is that I don't like how other people organise things. Growing
+        up has meant accepting that other people's way to organise things is
+        also ok.
       </p>
     </div>
   );
