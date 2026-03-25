7a7a4732213513895cb6bf8c442eb6b4763825aa
TheWombatConrad <wombatconrad@gmail.com>
2026-03-25 19:36:46 +0100

    attempt to fix safari photo stretching bug

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -1,22 +1,29 @@
-import "./IntroCard.css";
-import profilePic from "../assets/images/profile-pic.webp";
+import './IntroCard.css';
+import profilePic from '../assets/images/profile-pic.webp';
 
 const IntroCard = () => {
   return (
-    <div id="intro-card">
+    <div id='intro-card'>
       <div>
         <div>
           <h1>Malte Nilsson</h1>
           <h2>.NET Developer</h2>
         </div>
 
-        <img alt="Me, outside." src={profilePic} loading="eager" />
+        <div>
+          <img
+            alt='Me, outside.'
+            src={profilePic}
+            loading='eager'
+            id='maltes-photo'
+          />
+        </div>
       </div>
       <p
         style={{
-          background: "rgb(28, 27, 41, 0.7)",
-          padding: "5px",
-          textAlign: "center",
+          background: 'rgb(28, 27, 41, 0.7)',
+          padding: '5px',
+          textAlign: 'center',
         }}
       >
         I'm looking for a new job!
