c45cef3e451a588876eca3b3696ef3fbfb6c360c
TheWombatConrad <wombatconrad@gmail.com>
2026-03-25 21:06:13 +0100

    changed text

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,10 +5,11 @@ const AboutMe = () => {
       <p>
         I am currently working for PostNord as a mailmain. Before that, I've
         studied biology and programming. I'm inquisitive and am always learning
-        new things. My greatest strenght is that I'm very organised, my greatest
-        weakness is that I don't like how other people organise things. Growing
-        up has meant accepting that other people's way to organise things is
-        also ok.
+        new things. My greatest strength is that I'm very organised, my greatest
+        weakness is that I sometimes take on other people's organising. Growing
+        up has meant slowly getting better and better at working with others and
+        learning that things won't break just because it's not how I would do
+        it.
       </p>
     </div>
   );
