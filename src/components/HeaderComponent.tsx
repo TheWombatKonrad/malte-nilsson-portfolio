ef32d2f30fddc6422487154a7c1cdc096a0a00c3
TheWombatConrad <wombatconrad@gmail.com>
2026-03-25 19:47:08 +0100

    trying to fix safaribug

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,20 +1,16 @@
-import "./HeaderComponent.css";
-import profilePic from "../assets/images/profile-pic.webp"
+import './HeaderComponent.css';
+import profilePic from '../assets/images/profile-pic.webp';
 
 const HeaderComponent = () => {
   return (
     <header>
-      <div id="header-main">
+      <div id='header-main'>
         <div>
           <h1>Malte Nilsson</h1>
-          <h2>.NET Developer</h2>
+          <h2>.NET Developer 23</h2>
         </div>
 
-        <img
-          alt="Me, outside."
-          src={profilePic}
-          loading="eager"
-        />
+        <img alt='Me, outside.' src={profilePic} loading='eager' />
       </div>
       <p>I'm looking for a new job!</p>
     </header>
