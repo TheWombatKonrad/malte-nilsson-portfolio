08e8dcaeab8e3b873522145240768c46593b2dd5
TheWombatConrad <wombatconrad@gmail.com>
2026-03-26 19:37:53 +0100

    changed about me again

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,9 +7,7 @@ const AboutMe = () => {
         studied biology and programming. I'm inquisitive and am always learning
         new things. My greatest strength is that I'm very organised, my greatest
         weakness is that I sometimes take on other people's organising. Growing
-        up has meant slowly getting better and better at working with others and
-        learning that things won't break just because it's not how I would do
-        it.
+        up has meant learning to work together instead.
       </p>
     </div>
   );
